b3a3a86100b7b7cfe697b8254509258ff277da24
Payam Beigi <beigi.payam@gmail.com>
2022-12-11 16:24:27 -0800

    Check for Input and output UTXO, continue if balance enough

--- transaction.js
+++ transaction.js
@@ -4,9 +4,22 @@ class Transaction {
       this.outputUTXOs = outputUTXOs;
   }
   execute() {
+
       const txo = this.inputUTXOs.some(x => x.spent)
       if (txo) {
         throw new Error("Cannot include a TXO");
+      }
+
+      const totalInput = this.inputUTXOs.reduce((p, c) => {
+        return p + c.amount;
+      }, 0);
+
+      const totalOutput = this.outputUTXOs.reduce((p, c) => {
+        return p + c.amount;
+      }, 0);
+
+      if (totalInput < totalOutput) {
+        throw new Error("Not enough balance to continue");
       }
   }
 }
